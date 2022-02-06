a83e595f65f3e8d1961c6cbaf3f8599794789967
Juank544 <camilo.gv@live.com>
2022-02-06 17:29:53 -0500

    License - Heroku config

--- src/main/java/co/edu/escuelaing/App.java
+++ src/main/java/co/edu/escuelaing/App.java
@@ -9,6 +9,14 @@ import static spark.Spark.*;
 public class App 
 {
     public static void main( String[] args ){
+        port(getPort());
         get("/hello", (req, res) -> "Hello World");
+    }
+
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567; //returns default port if heroku-port isn't set(i.e. on localhost)
     }
 }
