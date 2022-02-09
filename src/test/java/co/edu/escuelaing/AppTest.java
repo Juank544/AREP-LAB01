440fd340ddb205ab15dae7c3c6d7d89bd65f0a5d
Juank544 <camilo.gv@live.com>
2022-02-08 21:39:31 -0500

    Tests C -> F

--- src/test/java/co/edu/escuelaing/AppTest.java
+++ src/test/java/co/edu/escuelaing/AppTest.java
@@ -1,7 +1,10 @@
 package co.edu.escuelaing;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import co.edu.escuelaing.service.TempService;
+import co.edu.escuelaing.service.impl.TempImpl;
 import org.junit.Test;
 
 /**
@@ -9,6 +12,12 @@ import org.junit.Test;
  */
 public class AppTest 
 {
+    TempService tempService;
+
+    public AppTest() {
+        this.tempService = new TempImpl();
+    }
+
     /**
      * Rigorous Test :-)
      */
@@ -16,5 +25,23 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void negativeCtoF(){
+        String res = String.valueOf(tempService.CelsiusToFahrenheit(-10));
+        assertEquals(res,"14.0");
+    }
+
+    @Test
+    public void ceroCtoF(){
+        String res = String.valueOf(tempService.CelsiusToFahrenheit(0));
+        assertEquals(res,"32.0");
+    }
+
+    @Test
+    public void positiveCtoF(){
+        String res = String.valueOf(tempService.CelsiusToFahrenheit(10));
+        assertEquals(res,"50.0");
     }
 }
